8e39c57e0ca69026b7e238a2e11519b1095f841d
tm-hack <takufrom1124@gmail.com>
2023-02-10 00:57:07 +0900

    最後まで書いてみたがエラーが出力される

---
+++ README.md
@@ -0,0 +1,6 @@
+# single_thread_webserver
+
+## Overview
+The Rust Programming Language 日本語版 20章の演習内容です。
+
+https://doc.rust-jp.rs/book-ja/ch20-00-final-project-a-web-server.html
---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+    /// スレッドプールを生成する
+    ///
+    /// * size: プール数
+    ///
+    /// ## Panic
+    ///
+    /// sizeが0ならパニックする。
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        // チャネルの送信者（sender）と受信者（receiver）を生成する
+        let (sender, receiver) = mpsc::channel();
+
+        // worker間でreceiverの所有権を共有化し、スレッド間で値を可変化する
+        // Arc型は複数のワーカーに受信者を所有させ、Mutexにより、1度に受信者から1つの仕事をたった1つのワーカーが受け取ることを保証する
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            let worker = Worker::new(id, Arc::clone(&receiver));
+            // 生成したworkerをworkersに挿入する
+            workers.push(worker);
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    // 引数で受け取ったジョブをworkerに引き渡していることは分かるが、
+    // ソースコードの中で行っていることの詳細は不明
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {} got a job: executing", id);
+
+            job.call_box();
+        });
+        Worker { id, thread }
+    }
+}
